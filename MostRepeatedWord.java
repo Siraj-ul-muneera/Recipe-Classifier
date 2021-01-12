ea75f93045e9b4cb34f04e1383c652f95bae716a
Siraj-ul-muneera <samreenmuneera@gmail.com>
2021-01-12 10:28:00 +0000

    Modified a file

--- MostRepeatedWord.java
+++ MostRepeatedWord.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class MostRepeatedWord {    
         
     public static void main (String[] args) {
-	    system.out.println("Hi!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	    System.out.println("Hi!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 }
 
